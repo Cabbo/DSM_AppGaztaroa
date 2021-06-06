4ab70bd92af29e8d4ad88c3fe6c1e60219609b1c
Iván <ivancaballero30@yahoo.com>
2021-06-06 13:09:37 +0200

    PF 1 Backend Firebase descarga

--- comun/comun.js
+++ comun/comun.js
@@ -1,5 +1,6 @@
-//export const baseUrl = 'http://192.168.67.127:3001/';
-export const baseUrl = 'http://192.168.68.86:3001/';
+export const baseUrl = 'https://dsm-test-6ee3d-default-rtdb.europe-west1.firebasedatabase.app/gaztaroa/';
+//export const baseUrl = 'http://192.168.0.13:3001/';
+//export const baseUrl = 'http://192.168.68.86:3001/';
 //export const baseUrl = 'http://192.168.1.50:3001/';
 export const colorGaztaroaOscuro = '#015afc';
 export const colorGaztaroaClaro = '#c2d3da';
